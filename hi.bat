665b755d2d19a32c58f84ef85ca1a5a7924b685a
CIT142F-13@m307.uc-bcf.edu.ph <CIT142F-13@m307.uc-bcf.edu.ph>
2022-09-14 17:10:45 +0800

    -batch upload

--- hi.bat
+++ hi.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/Jajauwuw/Cit14f.git
+git remote set-url origin https://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@github.com/Jajauwuw/Cit14f.git
 pause
 git add .
 git commit -m -"batch upload"
